cc51480a22438d51d3f864c1fb8d52234b13e340
Helen Huang <helen_huang@brown.edu>
2022-11-14 08:55:18 -0500

    did some work

--- scripts/main.py
+++ scripts/main.py
@@ -18,18 +18,27 @@ def clear_all():
     """ FOR TESTING. Removes all polygons created. """
     cmds.select(all=True)
     cmds.delete()
-    
+
+
+def create_ground(start_coord, end_coord):
+    print(start_coord)
+    print(end_coord)
+    width = abs(end_coord[0] - start_coord[0])
+    height = abs(end_coord[1] - start_coord[1])
+    cmds.polyPlane(width=width, height=height)
 
 def create_building_at_location(num=0, x=0, z=0):
     """
-    Creates a building of random height at specified location in scene
+    Creates a building of random height at specified location in scene. 
+    TODO: Currently just a single cube, but want to make a building be 1-3 cubes
 
     Parameters:
     num: building number used for naming
     x, y: location in world space to translate created building
     """
     scale = math.sqrt(x**2 + z**2) + 0.01
-    height = random.randrange(BUILDING_MIN_HEIGHT, BUILDING_MAX_HEIGHT) * min((8/scale), 1)
+    scale = min((5/scale), 1)
+    height = random.randrange(BUILDING_MIN_HEIGHT, BUILDING_MAX_HEIGHT) * scale
     width = random.randrange(BUILDING_MIN_WIDTH, BUILDING_MAX_WIDTH)
     depth = random.randrange(BUILDING_MIN_DEPTH, BUILDING_MAX_DEPTH)
 
@@ -39,9 +48,10 @@ def create_building_at_location(num=0, x=0, z=0):
     cmds.move(x, height/2, z)
     
     
-def create_city_block(start_coord=(10,10), width=3, depth=3, padding=1.2, num=0):
+def create_city_block(start_coord=(10,10), width=3, depth=3, padding=1.2, num=0) -> int:
     """
     Create a city block using the helper function create_building_at_location()
+    TODO: Space out the buildings better. Might need to redo algorithm
     
     Params:
     start_coord: (x,y) coordinate that represents the top-left coord of the grid
@@ -49,10 +59,13 @@ def create_city_block(start_coord=(10,10), width=3, depth=3, padding=1.2, num=0)
     depth: num buildings to create in the z-direction
     padding: padding factor between each building created
     """
-
-    # Configs
     start_x = start_coord[0]
     start_z = start_coord[1]
+
+    # scale = math.sqrt(start_x**2 + start_z**2) + 0.01
+    # scale = min((20/scale), 1)
+    # width = int(width * scale)
+    # depth = int(depth * scale)
     
     # Generate buildings in grid pattern
     for x in range(width):
@@ -64,21 +77,25 @@ def create_city_block(start_coord=(10,10), width=3, depth=3, padding=1.2, num=0)
 
 def create_city(street_pad=2, original_start_coord=(30, 30), width=10, depth=10):
     """
-    Create the full city using the helper function create_city_block().
+    Create the full city using the helper functions create_city_block() and
+    create_ground().
 
     Params:
     street_pad: padding between city blocks.
     """
     cmds.group(em=True, name='city') # Create parent group for all the cities
-    start_coord = original_start_coord
-    num = 0
-    for i in range(width):
+    curr_coord = original_start_coord
+    num = 1
+    min_depth = 1
+    for _ in range(width):
         rand_w = random.randrange(CITY_BLOCK_MIN_WIDTH, CITY_BLOCK_MAX_WIDTH)
-        for j in range(depth):
+        for _ in range(depth):
             rand_r = random.randrange(CITY_BLOCK_MIN_DEPTH, CITY_BLOCK_MAX_DEPTH)
-            num += create_city_block(start_coord=start_coord, width=rand_w, depth=rand_r, num=num)
-            start_coord=(start_coord[0], start_coord[1] - rand_r - street_pad)
-        start_coord=(start_coord[0] - rand_w - street_pad, original_start_coord[1])
+            num = create_city_block(start_coord=curr_coord, width=rand_w, depth=rand_r, num=num) + 1
+            curr_coord = (curr_coord[0], curr_coord[1] - rand_r - street_pad)
+            min_depth = min(min_depth, curr_coord[1])
+        curr_coord=(curr_coord[0] - rand_w - street_pad, original_start_coord[1])
+    create_ground(start_coord=original_start_coord, end_coord=(curr_coord[0], min_depth))
            
 
 def main():
